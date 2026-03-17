2ea195aee7350d539fc15e9732d69472f494f2f6
Raul <akhuliraul@gmail.com>
2026-03-17 22:28:50 +0530

    add a file to check strong number or not having TC O(log n) and SC O(1)

---
+++ ra_06_strong_number.cpp
@@ -0,0 +1,49 @@
+// strong
+
+
+# include <iostream>
+using namespace std;
+
+
+int factorial(int num){
+
+	if(num == 0) return 1;
+	if(num == 1) return 1;
+	
+	int fact = 1;
+	for(int i = 2; i <= num; i++){       // you can create a factorial array and access the elements index wise.
+	
+		fact = fact*i;
+	}
+	return fact;
+}
+
+
+
+
+int main(){
+
+	int n;
+	cout << "enter number:";
+	cin >> n;
+	int actual = n;
+	
+	int sum = 0;
+	while(n != 0){
+	
+		int digit = n % 10;
+		sum += factorial(digit);
+		n /= 10;
+	}
+	
+	if(actual == sum){
+	
+		cout << "entered number is strong.";
+	}
+	else{
+	
+		cout << "entered numbers is not strong.";
+	}
+	
+	return 0;
+}
